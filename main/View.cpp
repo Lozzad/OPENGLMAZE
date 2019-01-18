f651441240bde7ba35810134e22866bc56c0b77b
Lozzad <36076143+Lozzad@users.noreply.github.com>
2019-01-18 12:34:47 +0000

    Add files via upload

---
+++ main/View.cpp
@@ -0,0 +1,260 @@
+#include "View.hpp"
+
+View::View(int widtht, int heightt) {
+	width = widtht;
+	height = heightt;
+
+}
+View::~View() {
+
+}
+
+glm::mat4 getMVPMatrix(glm::mat4 ProjectionMatrix, glm::mat4 ViewMatrix, glm::mat4 ModelMatrix) {
+	glm::mat4 Projection = ProjectionMatrix;
+	glm::mat4 View = ViewMatrix;
+	glm::mat4 Model = ModelMatrix;
+	glm::mat4 mvp = Projection * View * Model;
+
+	return mvp;
+}
+
+int View::initialise() {
+
+	// Initialise GLFW
+	if (!glfwInit())
+	{
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		getchar();
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+
+	// Open a window and create its OpenGL context
+	window = glfwCreateWindow(width, height, "My Game", NULL, NULL);
+	if (window == NULL) {
+		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	//for getting the esc key
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	//hide mouse and enable unlimited movement
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	//set mouse at center screen
+	glfwPollEvents();
+	glfwSetCursorPos(window, width / 2, height / 2);
+
+	// Initialize GLEW
+	glewExperimental = GL_TRUE; //needed for core profile
+
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	// Set the mouse at the center of the screen
+
+	glfwPollEvents();
+	//we set the cursor at the centre so that it always start on our origin (0,0,0)
+	glfwSetCursorPos(window, width / 2, height / 2);
+
+
+	// pink background
+	glClearColor(0.8f, 0.5f, 0.8f, 0.0f);
+
+	//define vertex array
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	//vertex info for square
+	static const GLfloat g_vertex_buffer_data[] = {
+		-1.0f,-1.0f,-1.0f, // triangle 1 : begin
+	-1.0f,-1.0f, 1.0f,
+	-1.0f, 1.0f, 1.0f, // triangle 1 : end
+	1.0f, 1.0f,-1.0f, // triangle 2 : begin
+	-1.0f,-1.0f,-1.0f,
+	-1.0f, 1.0f,-1.0f, // triangle 2 : end
+	1.0f,-1.0f, 1.0f,
+	-1.0f,-1.0f,-1.0f,
+	1.0f,-1.0f,-1.0f,
+	1.0f, 1.0f,-1.0f,
+	1.0f,-1.0f,-1.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f, 1.0f, 1.0f,
+	-1.0f, 1.0f,-1.0f,
+	1.0f,-1.0f, 1.0f,
+	-1.0f,-1.0f, 1.0f,
+	-1.0f,-1.0f,-1.0f,
+	-1.0f, 1.0f, 1.0f,
+	-1.0f,-1.0f, 1.0f,
+	1.0f,-1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f,-1.0f,-1.0f,
+	1.0f, 1.0f,-1.0f,
+	1.0f,-1.0f,-1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f,-1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f,-1.0f,
+	-1.0f, 1.0f,-1.0f,
+	1.0f, 1.0f, 1.0f,
+	-1.0f, 1.0f,-1.0f,
+	-1.0f, 1.0f, 1.0f,
+	1.0f, 1.0f, 1.0f,
+	-1.0f, 1.0f, 1.0f,
+	1.0f,-1.0f, 1.0f
+	};
+
+	//send vert info to GL using vertex buffer
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	
+
+	// One color for each vertex. They were generated randomly.
+	static const GLfloat g_color_buffer_data[] = {
+		0.583f,  0.771f,  0.014f,
+		0.609f,  0.115f,  0.436f,
+		0.327f,  0.483f,  0.844f,
+		0.822f,  0.569f,  0.201f,
+		0.435f,  0.602f,  0.223f,
+		0.310f,  0.747f,  0.185f,
+		0.597f,  0.770f,  0.761f,
+		0.559f,  0.436f,  0.730f,
+		0.359f,  0.583f,  0.152f,
+		0.483f,  0.596f,  0.789f,
+		0.559f,  0.861f,  0.639f,
+		0.195f,  0.548f,  0.859f,
+		0.014f,  0.184f,  0.576f,
+		0.771f,  0.328f,  0.970f,
+		0.406f,  0.615f,  0.116f,
+		0.676f,  0.977f,  0.133f,
+		0.971f,  0.572f,  0.833f,
+		0.140f,  0.616f,  0.489f,
+		0.997f,  0.513f,  0.064f,
+		0.945f,  0.719f,  0.592f,
+		0.543f,  0.021f,  0.978f,
+		0.279f,  0.317f,  0.505f,
+		0.167f,  0.620f,  0.077f,
+		0.347f,  0.857f,  0.137f,
+		0.055f,  0.953f,  0.042f,
+		0.714f,  0.505f,  0.345f,
+		0.783f,  0.290f,  0.734f,
+		0.722f,  0.645f,  0.174f,
+		0.302f,  0.455f,  0.848f,
+		0.225f,  0.587f,  0.040f,
+		0.517f,  0.713f,  0.338f,
+		0.053f,  0.959f,  0.120f,
+		0.393f,  0.621f,  0.362f,
+		0.673f,  0.211f,  0.457f,
+		0.820f,  0.883f,  0.371f,
+		0.982f,  0.099f,  0.879f
+	};
+
+	glGenBuffers(1, &colorbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+	
+	programID = LoadShaders("SimpleVertexShader.hlsl", "SimpleFragmentShader.hlsl");
+
+
+
+	return 1;
+}
+
+void View::update() {
+
+	do {
+		// Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		MatrixID1 = glGetUniformLocation(programID, "MVP");
+		computeMatricesFromInputs(window, height, width);
+
+		glm::mat4 mvp1 = getMVPMatrix(
+			getProjectionMatrix(),
+			getViewMatrix(),
+			glm::mat4(1.0f)
+		);
+		glUniformMatrix4fv(MatrixID1, 1, GL_FALSE, &mvp1[0][0]);
+
+		//use our shaders
+		glUseProgram(programID);
+
+		//1st attribute buffer is verts
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,			//key, must be same as vertex shader
+			3,			//size
+			GL_FLOAT,	//type
+			GL_FALSE,	//normalised?
+			0,			//stride
+			(void*)0	//array buffer offset
+		);
+
+		// 2nd attribute buffer : colors
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+		glVertexAttribPointer(
+			1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+			3,                                // size
+			GL_FLOAT,                         // type
+			GL_FALSE,                         // normalized?
+			0,                                // stride
+			(void*)0                          // array buffer offset
+		);
+		
+		//enable depth test 
+		glEnable(GL_DEPTH_TEST);
+		//accept frag if closer
+		glDepthFunc(GL_LESS);
+
+		//the triangle is then drawn
+		glDrawArrays(GL_TRIANGLES, 0, 12*3);
+
+		glDisableVertexAttribArray(0);
+
+		glDisableVertexAttribArray(1);
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} // Check if the ESC key was pressed or the window was closed
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		glfwWindowShouldClose(window) == 0);
+
+	//cleanup VBO and shader
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteBuffers(1, &colorbuffer);
+
+	
+	glDeleteProgram(programID);
+	glDeleteTextures(1, &Texture);
+	glDeleteVertexArrays(1, &VertexArrayID);
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
+
+
+}
+
+
---
+++ main/View.hpp
@@ -0,0 +1,51 @@
+#pragma once
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <glfw3.h>
+#include <vector>
+
+// Include GLM
+#include <glm/glm.hpp>
+using namespace glm;
+#include "common/objloader.hpp"
+#include "common/vboindexer.hpp"
+#include <glm/gtx/transform.hpp>
+#include "common/shader.hpp"
+#include "common/controls.hpp"
+#include "common/text2D.hpp"
+#include "common/texture.hpp"
+#include <glm/gtc/matrix_transform.hpp>
+
+class View {
+private:
+    
+    GLFWwindow* window;
+    
+    GLuint programID;
+  
+    
+    GLuint VertexArrayID;
+	GLuint vertexbuffer;
+	GLuint colorbuffer;
+	GLuint Texture;
+	GLuint TextureID;
+
+	GLuint MatrixID1;
+
+    
+    //screen size
+    int width;
+    int height;
+  
+    
+    
+    
+public:
+    View(int widtht, int heightt);
+    ~View();
+    int initialise();
+    void update();    
+};
+
